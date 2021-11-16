e73602881caaba751237ac8e538aaffae4bf842b
Ben Kim <pluckyman@gmail.com>
2021-11-16 12:00:47 +0900

    redox grv set

--- keyboards/redox/keymaps/default/keymap.c
+++ keyboards/redox/keymaps/default/keymap.c
@@ -33,6 +33,8 @@ enum custom_keycodes {
   VIM_WINDOW_DOWN,
   VIM_FUZZY_FIND,
   LCTRL_W,
+  LCTRL_E,
+  LCTRL_Q,
   LCTRL_A,
   LCTRL_S,
   LCTRL_X,
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_EQL ,
+     KC_GRV , KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_EQL ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,OPTION_O,                          KC_RALT ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      SUPER_GRV,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,_______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,_______ ,LCTRL_W ,_______ ,LCTRL_R ,LCTRL_T ,KC__VOLDOWN,                      KC__VOLUP,KC_SLBRC,KC_SRBRC,KC_P8   ,KC_P9   ,KC_LBRC ,KC_RBRC,
+     _______ ,LCTRL_Q ,LCTRL_W ,LCTRL_E ,LCTRL_R ,LCTRL_T ,KC__VOLDOWN,                      KC__VOLUP,KC_SLBRC,KC_SRBRC,KC_P8   ,KC_P9   ,KC_LBRC ,KC_RBRC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______ ,LCTRL_A ,LCTRL_S ,LCTRL_D ,LCTRL_F ,LCTRL_G ,KC__MUTE,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,_______ ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -255,6 +257,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case LCTRL_W:
       LCTRL_CHARACTER_PRESS(w)
+    case LCTRL_E:
+      LCTRL_CHARACTER_PRESS(e)
+    case LCTRL_Q:
+      LCTRL_CHARACTER_PRESS(q)
     case LCTRL_A:
       LCTRL_CHARACTER_PRESS(a)
     case LCTRL_S:
