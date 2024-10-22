17c9706981b235359af8e61701cf118cdc93549b
Ben Kim <pluckyman@gmail.com>
2024-10-23 00:47:33 +0900

    Small change for ergodox

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC__VOLDOWN,                   KC__VOLUP, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC__VOLDOWN,                   KC__VOLUP, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    OPTION_O,                        KC_RALT, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,                          KC_MINS, KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDN,                       LCTRL_RBRC, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SUPER_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC__VOLDOWN,                  KC__VOLUP, KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12, \
     _______,   LCTRL_Q, LCTRL_W, LCTRL_E, LCTRL_R, LCTRL_T, KC__MUTE,                       _______, KC_SLBRC, KC_SRBRC,KC_P8,   KC_P9,    KC_LBRC, KC_RBRC, \
     _______,   LCTRL_A, LCTRL_S, LCTRL_D, LCTRL_F, LCTRL_G, _______,                        _______, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-    _______,   LCTRL_Z, LCTRL_X, LCTRL_C, LCTRL_V, LCTRL_B, KC_PGUP,                        _______, CH_PREV_TAB,  _______, _______, CH_NEXT_TAB,  _______, _______, \
+    _______,   LCTRL_Z, LCTRL_X, LCTRL_C, LCTRL_V, LCTRL_B, KC_PGUP,                        LCTRL_T, CH_PREV_TAB,  _______, _______, CH_NEXT_TAB,  _______, _______, \
     _______,   _______, _______, _______,          LOWER,   _______, _______,      _______, _______, RAISE,             KC_HOME, KC_PGDN,  KC_PGUP, KC_END   \
   ),
 
